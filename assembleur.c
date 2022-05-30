30c5f0ab873c6f497712a872c9348acba98d7d12
Antoine A <59095838+Piazo@users.noreply.github.com>
2022-05-30 12:25:25 +0200

    test

--- assembleur.c
+++ assembleur.c
@@ -2,38 +2,51 @@
 
 #define MAX_TABLESIZE 2000
 instruction tab_instruction[MAX_TABLESIZE];
-char* operator;
 
-int index_tab = 0;
-int var
+int index_last_instr_tab = 0;
+int index_current_instr = 0;
 
-void add_instruc_to_tab(char* nom_ope, int r1, int r2, int r3){
-    struct instruction new_instr = {nom_ope, r1, r2, r3};
-    tab_instruction[index_tab] = new_instr;
-    index_tab++;
+void add_instruc_to_tab(char* nom_ope, int r0, int r1, int r2){
+    struct instruction new_instr = {nom_ope, r0, r1, r2};
+    tab_instruction[index_last_instr_tab] = new_instr;
+    index_last_instr_tab++;
 }
 
 int get_index (){
-    return index_tab;
+    return index_last_instr_tab;
 }
 
-void select_operator (char* op){
-    operator = op;
-}
+void interpreter(){
+    while(index_current_instr<index_last_instr_tab){
+        char operator[5],
+        strcpy(operator, tab_instruction[index_current_instr].operation);
+        int r0 = tab_inter[index_current_instr].r0;
+		int r1 = tab_inter[index_current_instr].r1;
+		int r2 = tab_inter[index_current_instr].r2;
 
-void ope_sur_nb(int nb){
-    if (operator == NULL){
-        add_instruct_to_tab("AFC", )
+        if (!strcmp(op, "ADD")) {
+			registre[r0] = registre[r1] + registre[r2];
+        }
+        else if (!strcmp(op, "MUL")) {
+			registre[r0] = registre[r2] * registre[r1];
+		}
+        else if (!strcmp(op, "SOU")) {
+			registre[r0] = registre[r2] - registre[r1];
+		}
+        else if (!strcmp(op, "DIV")) {
+			registre[r0] = registre[r2] / registre[r1];
+		}
+        else if (!strcmp(op, "COP")) {
+			registre[r0] = r1;
+		}
+        else if (!strcmp(op, "AFC")) {
+			registre[r0] = r1;
+		}
+
+        index_current_instr++;
+
     }
 }
 
-void ope_sur_var(char* nom_var){
-    int adr = get_adr(name_variable);
-    if (operator == NULL){
-        add_instruct_to_tab("COP", target_affect, adr, -1);
-    }
-    else{
-        add_instruction(operator, target_affect, target_affect, adr);
-    }
 }
 
--- assembleur.h
+++ assembleur.h
@@ -1,7 +1,7 @@
 #ifndef ASM_H
 #define ASM_H
 
-enum opcode {ADD,MUL,SOU,DIV,COP,AFC,AFCA,JMP,JMF};
+enum opcode {ADD,MUL,SOU,DIV,COP,AFC,JMP,JMF,INF,SUP,EQU,PRI};
 
 typedef struct{
     char* operation;
@@ -10,6 +10,13 @@ typedef struct{
     int r2;
 }instruction;
 
-void add_instruc_to_tab(enum opcode operation, int r1, int r2, int r3);
+//Ajoute une instruction au tableau des instructions a effectuer
+void add_instruc_to_tab(enum opcode operation, int r0, int r1, int r2);
+
+//Retourne l'index de la derniere instruction dans le tableau des instructions 
+int get_index ();
+
+
+
 
 #endif
